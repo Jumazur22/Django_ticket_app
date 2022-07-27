3d9b471a57a45a76c10260e174bf7308c8e8eb03
Jumazur22 <juliamazur22@gmail.com>
2022-07-27 14:40:53 +0100

    Added a database with two tables

--- uniproject/ticket_app/models.py
+++ uniproject/ticket_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.db.models import CharField
 
-# Create your models here.
+class Ticket(models.Model):
+    issue_type = CharField(("Area of Problem"), max_length=50)
+    issue_description = models.TextField(max_length=300)
+    date_of_field = models.DateTimeField(("Date of when the issue occured"), auto_now=False, auto_now_add=False)
+
+
+class User(models.Model):
+    name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254)
+    ticket = models.ForeignKey( Ticket, on_delete=models.CASCADE)
