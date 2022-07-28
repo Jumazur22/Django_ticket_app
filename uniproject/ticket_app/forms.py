19e3da8e40f02f4be4ae054a8803cd6c0bdd3184
Jumazur22 <juliamazur22@gmail.com>
2022-07-28 12:12:57 +0100

    Formatted date and time field

--- uniproject/ticket_app/forms.py
+++ uniproject/ticket_app/forms.py
@@ -3,10 +3,14 @@ from django import forms
 from .models import Ticket
 
 class TicketForm(forms.ModelForm):
+    date = forms.DateTimeField(
+        label = 'Date & Time',
+        widget = forms.widgets.DateTimeInput(attrs={'type':'datetime-local'}),
+    )
     class Meta:
         model = Ticket
         fields = [
             'issue_type',
             'issue_description',
-            'date_of_field'
+            'date'
         ]
--- uniproject/ticket_app/models.py
+++ uniproject/ticket_app/models.py
@@ -4,8 +4,11 @@ from django.db.models import CharField
 class Ticket(models.Model):
     issue_type = CharField(("Area of Problem"), max_length=50)
     issue_description = models.TextField(max_length=300)
-    date_of_field = models.DateTimeField(("Date of when the issue occured"), auto_now=False, auto_now_add=False)
+    date = models.DateTimeField(("Date of when the issue occured"))
 
+    def __str__(self):
+        return self.Ticket
+    
 class User(models.Model):
     name = models.CharField(max_length=50)
     surname = models.CharField(max_length=50)
