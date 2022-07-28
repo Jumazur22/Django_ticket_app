47b0e963d8160a25300e04bfee4528adde55f4c4
Jumazur22 <juliamazur22@gmail.com>
2022-07-28 11:45:12 +0100

    made a html page to render TicketForm

--- uniproject/ticket_app/urls.py
+++ uniproject/ticket_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 #This calls a view by mapping it to a url
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    path('create/', views.create_ticket, name="create_ticket")
 ]
