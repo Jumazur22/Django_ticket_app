bfe333cc00beff4c2eee63f66696d989fa91227b
Jumazur22 <juliamazur22@gmail.com>
2022-08-15 16:36:57 +0100

    Added some comments

--- uniproject/ticket_app/views.py
+++ uniproject/ticket_app/views.py
@@ -4,9 +4,12 @@ from django.http import HttpResponse
 from .models import Ticket
 from .forms import TicketForm
 
+# Here its rendering the index.html view
 def index(request):
     return render(request, "index.html")    
 
+# This function sets the form variable for the TicketForm
+# and checks if the form is valid and if it is then it gets saved to the datbase
 def create_ticket(request):
     form = TicketForm(request.POST or None)
     if form.is_valid():
