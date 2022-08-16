b192b55b413bd6f50bf466ce67e1588405ef7b92
Jumazur22 <juliamazur22@gmail.com>
2022-08-16 15:54:03 +0100

    Added a avatar from logged in person

--- uniproject/ticket_app/urls.py
+++ uniproject/ticket_app/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('create/', views.create_ticket, name="create_ticket"),
     path('register', views.register_request, name='register'),
-    path('login', views.login_request, name='login')
+    path('login', views.login_request, name='login'),
+    path("logout", views.logout_request, name= "logout"),
 ]
