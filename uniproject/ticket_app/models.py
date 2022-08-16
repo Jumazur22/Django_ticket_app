c20214f73513c954ecc85970b384b619750278a9
Jumazur22 <juliamazur22@gmail.com>
2022-08-16 10:03:31 +0100

    Delete user as django has built in user model

--- uniproject/ticket_app/models.py
+++ uniproject/ticket_app/models.py
@@ -5,9 +5,3 @@ class Ticket(models.Model):
     issue_type = CharField(("Area of Problem"), max_length=50)
     issue_description = models.TextField(max_length=300)
     date = models.DateTimeField()
-
-class User(models.Model):
-    name = models.CharField(max_length=50)
-    surname = models.CharField(max_length=50)
-    email = models.EmailField(max_length=254)
-    ticket = models.ForeignKey( Ticket, on_delete=models.CASCADE)
