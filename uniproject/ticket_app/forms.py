6041199ec9863aae2433fb2d16ec6980a44c9155
Jumazur22 <juliamazur22@gmail.com>
2022-08-16 10:19:14 +0100

    Added a user form that connects to a user model

--- uniproject/ticket_app/forms.py
+++ uniproject/ticket_app/forms.py
@@ -1,7 +1,11 @@
 from django import forms
-
 from .models import Ticket
 
+# Django has a pre-build model User
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+# This is a form to open a case to create a ticket
 class TicketForm(forms.ModelForm):
     date = forms.DateTimeField(
         label = 'Date & Time',
@@ -20,3 +24,23 @@ class TicketForm(forms.ModelForm):
             'issue_description',
             'date'
         ]
+
+# Here we are creating a user registration form
+class NewUserForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = (
+            "username",
+            "email",
+            "password1",
+            "password2"
+        )
+    
+    def save(self, commit=True):
+        user = super(NewUserForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
