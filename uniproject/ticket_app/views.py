ecd05c27a0cb7bba317164015a1113d1356eb897
Jumazur22 <juliamazur22@gmail.com>
2022-08-30 11:26:10 +0100

    Once logged out go back to register page

--- uniproject/ticket_app/views.py
+++ uniproject/ticket_app/views.py
@@ -61,4 +61,4 @@ def login_request(request):
 def logout_request(request):
     logout(request)
     messages.info(request, "You have successfully logged out.")
-    return redirect("index")
+    return redirect("register")
